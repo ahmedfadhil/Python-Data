812473bf2e5af6137b30911dc45636e0831af463
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-11-11 14:38:21 +0100

    last commit

--- scaling.py
+++ scaling.py
@@ -45,4 +45,17 @@ grades = df['Grades'].astype('category', categories=[
 ], ordered=True)
 
 print(grades.head())
-print(grades>'D')
+print(grades > 'D')
+
+df = pd.read_csv('census.csv')
+df = df[df['SUMLEV'] == 50]
+df = df.set_index('STNAME').groupby(level=0)['CENSUS2010POP'].agg({'avg': np.average})
+pd.cut(df['avg'], 10)
+
+s = pd.Series([168, 180, 174, 190, 170, 185, 179, 181, 175, 169, 182, 177, 180, 171])
+
+pd.cut(s, 3)
+
+# You can also add labels for the sizes [Small < Medium < Large].
+whiki = pd.cut(s, 3, labels=['Small', 'Medium', 'Large'])
+print(whiki)
